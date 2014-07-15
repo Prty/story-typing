192872a5c344cf55c6286dcc47bf29b88e7275ac
Moises Sanabria <sesmoi.data@gmail.com>
2014-07-15 14:37:53 -0400

    first commit adding working story-type to heroku

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+// CREATE TOKEN FIELD
+$('textarea.email').tokenField();
+
+// OBSERVED WORDS ARRAY
+var observed_WORDS = [
+	'dog',
+	'cat',
+	'tree',
+	'gold',
+	'fun',
+	'super',
+	'running',
+	'super',
+	'cool',
+	'pirate'
+];
